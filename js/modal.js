640e4bc20602ebed6a55b8d673e9c26ca5b99d04
MoMcGee <mo.mcgee@gmail.com>
2020-06-25 19:45:53 +0300

    Исправляем анимацию окна поиска гостиниц
    Добавлена анимация закрытия окна.

--- js/modal.js
+++ js/modal.js
@@ -11,21 +11,27 @@ var storage = "";
 
 try {
   storage = localStorage.getItem("adult");
-  console.log(arrival);
 } catch (err) {
   isStorageSupport = false;
 }
 
 openFormButton.addEventListener("click", function () {
-  modal.classList.toggle("modal_show");
-  modal.classList.toggle("modal_hide");
-  modal.classList.remove("modal_error");
-  arrivalDate.focus();
-  if (storage) {
-    adultNumbers.value = storage;
-    childNumbers.value = storage;
+  if (modal.classList[1] === "modal_show") {
+    modal.classList.add("modal_go-up");
+    setTimeout(function() {modal.classList.remove("modal_show");},500);
+    modal.classList.remove("modal_error");
+  } else {
+    modal.classList.remove("modal_go-up");
+    modal.classList.add("modal_show");
+
+      arrivalDate.focus();
+      if (storage) {
+        adultNumbers.value = storage;
+        childNumbers.value = storage;
+      }
   }
 });
+
 
 searchForm.addEventListener("submit", function (evt) {
   if (!arrivalDate.value || !departureDate.value || !adultNumbers.value) {
@@ -45,7 +51,8 @@ window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (modal.classList.contains("modal_show")) {
       evt.preventDefault();
-      modal.classList.remove("modal_show");
+      modal.classList.add("modal_go-up");
+      setTimeout(function() {modal.classList.remove("modal_show");},500);
       modal.classList.remove("modal_error");
     }
   }
